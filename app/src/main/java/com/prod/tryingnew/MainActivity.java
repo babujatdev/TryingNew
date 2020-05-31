a9222a26220d6cb4eaf31a767870b21ec61ce065
babujdev <babujatdev@gmail.com>
2020-05-30 23:22:52 -0700

    added customview

--- app/src/main/java/com/prod/tryingnew/MainActivity.java
+++ app/src/main/java/com/prod/tryingnew/MainActivity.java
@@ -1,14 +1,25 @@
 package com.prod.tryingnew;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.prod.tryingnew.views.CustomViews;
 
 public class MainActivity extends AppCompatActivity {
+    CustomViews customViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init() {
+        customViews = (CustomViews) findViewById(R.id.customButtonView);
+        
+
     }
 }
